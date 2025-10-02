8e4d15e66d832ce4394dd2f1268c4d6933941679
Dang Quoc Binh Trinh <119688407+tr1nhtr1nh@users.noreply.github.com>
2025-10-02 12:39:40 +0200

    Tried to add vscode server support, but failed

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
-#references:
+# references:
 # https://wiki.nixos.org/wiki/Flakes 
 # https://discourse.nixos.org/t/allow-unfree-in-flakes/29904
+# nixos-vscode-server support (failed): https://github.com/nix-community/nixos-vscode-server
 
 {
   description = "A flake file for the master's thesis project";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+    #vscode-server.url = "github:nix-community/nixos-vscode-server";
+   };
 
   outputs = { self, nixpkgs }:
     let
@@ -16,8 +18,8 @@
       # need to add "config.allowUnfree = true" for sratoolkit
       pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in {
-      # For global installation run: nix profile install .#git
-      # or nix profile install nixpkgs#git
+      # For global installation run: nix profile install .#git (with this flake.nix)
+      # or use profile install nixpkgs#git
       packages.${system}.git = pkgs.git;
 
       devShells.${system}.default = pkgs.mkShell {
@@ -27,5 +29,13 @@
           echo "Welcome to the development shell!"
    	'';
       };
+     # nixosConfigurations.pumpkin03 = nixpkgs.lib.nixosSystem {
+     #   modules = [
+     #     vscode-server.nixosModules.default
+     #     ({ config, pkgs, ... }: {
+     #       services.vscode-server.enable = true;
+     #     })
+     #   ];
+     # };
     };
 }
