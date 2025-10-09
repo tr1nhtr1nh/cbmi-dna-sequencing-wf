3daeba1ec5e8f63641e0dfe97d593487479b9ca2
Dang Quoc Binh Trinh <s0590971@htw-berlin.de>
2025-10-09 12:25:55 +0200

    Added pigz pkg and formatting

--- flake.nix
+++ flake.nix
@@ -21,7 +21,13 @@
 
       devShells.${system}.default = pkgs.mkShell {
         # Add more tools here, only avaiable in devShell, make sure to choose a pkgs
-        buildInputs = [ pkgs.git pkgs.nextflow pkgs.singularity pkgs.sratoolkit pkgs.pigz];
+        buildInputs = [ 
+          pkgs.git 
+          pkgs.nextflow 
+          pkgs.singularity 
+          pkgs.sratoolkit 
+          pkgs.pigz
+        ];
         shellHook = ''
           echo "Welcome to the development shell for the ngs pipeline project!"
    	    '';
