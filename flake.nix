3f49898869cd7e3edbd5a9179d9466220278df5e
Dang Quoc Binh Trinh <s0590971@htw-berlin.de>
2025-10-14 12:15:15 +0200

    Removed unused variable

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
    };
 
-  outputs = { self, nixpkgs, kraken2, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
